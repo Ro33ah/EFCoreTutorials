936819b96c67ebb771c8f90692ce6420479f3a6b
Ro33ah <Rolzies@gmail.com>
2019-04-17 22:29:22 +0200

    message

--- EFCoreTutorials/EFCoreTutorials/DataInfoContext.cs
+++ EFCoreTutorials/EFCoreTutorials/DataInfoContext.cs
@@ -11,7 +11,7 @@ namespace EFCoreTutorials
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS; Database=DataInfoDB;Trusted_COnnection=True;");
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS; Database=DataInfoDB;Trusted_Connection=True;");
         }
     }
 }
